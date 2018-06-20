11ec31cea24da21f6ba8eefe84a9f816b1d56b45
Carter <38448166+c-rter@users.noreply.github.com>
2018-06-20 11:41:24 -0400

    Working model
    Still need 1st round timer fix

--- game.js
+++ game.js
@@ -1,23 +1,37 @@
-var questions = ["What is the blah blah question one?", 
-"What is the bleh bleh question two?", 
-"What is the blippidy blop question three?", "Question 4 Right here"];
+var questions = ["What movie won best picture in 2010?", 
+"What movie won best picture in 2011?", 
+"What movie won best picture in 2012?", "What movie won best picture in 2013?"];
 
-var button1answers = ["correct", "answer1question2", "answer1question3", "hello"];
-var button2answers = ["rawwaraw", "sdfasdf", "correct", "hello"];
-var button3answers = ["answer3question1", "correct", "answer3question3", "hello"];
-var button4answers = ["answer4question1", "answer4question2", "answer4question3", "correct"];
+var button1answers = ["The King's Speech", "Black Swan", "Inception", "The Fighter"];
+var button2answers = ["Zero Dark Thirty", "Django Unchained", "Argo", "Lincoln"];
+var button3answers = ["The Help", "The Artist", "Hugo", "The Tree of Life"];
+var button4answers = ["The Wolf of Wall Street", "Gravity", "Philomena", "12 Years a Slave"];
 
 
 var counter = 0;
 var myTimer;
-var correct = 0;
-var incorrect = 0;
+var countdownTimer;
 var message;
+var countdown = 7;
+
 
 $("#resultsSection").hide();
 
+function bigTimer () {
+    if (countdown <= 0) {
+        clearInterval(countdownTimer);
+        countdown = 7;
+    }
+    $("#timeRemaining").text(countdown);
+    countdown--;
+}
+
 function initTimedText() {
     $("#startButton").hide();
+    countdownTimer = setInterval(bigTimer, 1000);
+    $("#timeRemaining").show();
+    $("#mainQuestion").show();
+    $("#buttonSection").show();
     $("#mainQuestion").text(questions[counter]);
     $("#firstAnswer").text(button1answers[counter]);
     $("#secondAnswer").text(button2answers[counter]);
@@ -27,24 +41,25 @@ function initTimedText() {
 }
 
 function tooSlow () {
-alert("you are so slow, i am ashamed of you");
+alert("You have taken too much time.");
 middleScreen();
 }
 
 function middleScreen (inputTest) {
     clearTimeout(myTimer);
+    $("#timeRemaining").hide();
     $("#buttonSection").hide();
     $("#resultsSection").show();
 
     if (counter == 0) { // Question 1
 
         if (inputTest == 1){
-        var message = "You Are The Best!!!!";
+        var message = "Well done, you have a great movie memory!";
         $("#winLose").text(message);
         $("#correctAnswer").text(button1answers[0]);
         }
         else {
-        var message = "You Are An Idiot!!!!";
+        var message = "Difficult isn't it! Who has space in their brain for this stuff anyways...";
         $("#winLose").text(message);
         $("#correctAnswer").text(button1answers[0]);
         }
@@ -54,12 +69,12 @@ function middleScreen (inputTest) {
     if (counter == 1) { // Question 2
 
         if (inputTest == 3){
-        var message = "You Are The Best!!!!";
+        var message = "Well done, you have a great movie memory!";
         $("#winLose").text(message);
         $("#correctAnswer").text(button3answers[1]);
         }
         else {
-        var message = "You Are An Idiot!!!!";
+        var message = "Difficult isn't it! Who has space in their brain for this stuff anyways...";
         $("#winLose").text(message);
         $("#correctAnswer").text(button3answers[1]);
         }
@@ -69,12 +84,12 @@ function middleScreen (inputTest) {
     if (counter == 2) { // Question 3
 
         if (inputTest == 2){
-            var message = "You Are The Best!!!!";
+            var message = "Well done, you have a great movie memory!";
             $("#winLose").text(message);
             $("#correctAnswer").text(button2answers[2]);
         }
         else {
-            var message = "You Are An Idiot!!!!";
+            var message = "Difficult isn't it! Who has space in their brain for this stuff anyways...";
             $("#winLose").text(message);
             $("#correctAnswer").text(button2answers[2]);
         }
@@ -84,30 +99,45 @@ function middleScreen (inputTest) {
     if (counter == 3) { // Question 4
 
         if (inputTest == 4){
-            var message = "You Are The Best!!!!";
+            var message = "Well done, you have a great movie memory!";
             $("#winLose").text(message);
             $("#correctAnswer").text(button4answers[3]);
         }
         else {
-            var message = "You Are An Idiot!!!!";
+            var message = "Difficult isn't it! Who has space in their brain for this stuff anyways...";
             $("#winLose").text(message);
             $("#correctAnswer").text(button4answers[3]);
         }
 
     }
 
-    setTimeout(inPlayTimedText, 7000);
+    if (counter < 3)
+    {
+        setTimeout(inPlayTimedText, 7000);
+    }
+
+    if (counter == 3)
+    {
+        setTimeout(resetGame, 7000);
+    }
+
+}
+
+function resetGame () {
+    clearTimeout(myTimer);
+    alert("Game over. Press the start button to play again.");
+    counter = 0;
+    $("#resultsSection").hide();
+    $("#mainQuestion").hide();
+    $("#buttonSection").hide();
+    $("#startButton").show();
+
 }
 
 function inPlayTimedText() {
-
+    countdownTimer = setInterval(bigTimer, 1000);
+    $("#timeRemaining").show();
     counter++;
-
-    if (counter == 4)
-    {
-alert ("The end");
-    }
-
     $("#resultsSection").hide();
     $("#buttonSection").show();
     $("#mainQuestion").text(questions[counter]);
